e69490d4f4ab566bb687e3eba3ca61e9e9333497
ilikeschecters <hamiltia@mail.gvsu.edu>
2018-06-18 16:39:29 -0400

    changed from double pointers to global struct for myMaze

--- mazeSolver.cpp
+++ mazeSolver.cpp
@@ -13,19 +13,21 @@ std::vector<int> GetClosestNodeToFinish(int i, int j, int endX, int endY);
 int divideAndConquerMazeSolver(int i, int j);
 int dynamicProgrammingMazeSolver(int i, int j);
 int randomizedMazeSolver(int i, int j);
-bool isEmpty(char **myMaze, int i, int j, int height, int width);
-bool hasBeenChecked(char **myMaze, int i, int j, int height, int width);
-int bruteForceMazeSolver(char **myMaze, int i, int j, bool oneShot, int startX, int startY, struct maze structMaze);
-void printArray(char **array, int row, int col);
-bool bruteCheckForEmpty(char **myMaze, int i, int j, struct maze structMaze);
-bool bruteCheckForTraveled(char **myMaze, int i, int j, struct maze structMaze);
-bool isFinishAdjacent(char **myMaze, int i, int j);
+bool isEmpty(int i, int j);
+bool hasBeenChecked(int i, int j);
+int bruteForceMazeSolver(int i, int j, bool oneShot, int startX, int startY);
+void printArray(struct maze myMaze);
+bool bruteCheckForEmpty(int i, int j);
+bool bruteCheckForTraveled(int i, int j);
+bool isFinishAdjacent(int i, int j);
 
 struct maze
 {
-	int rows;
-	int cols;
-	char matrix [100][100];
+    int rows;
+    int cols;
+		int startX;
+		int startY;
+    char matrix [100][100];
 };
 
 maze myMaze;
@@ -33,189 +35,192 @@ int bruteForceCount = 0;
 
 int main()
 {
-	//required variables
-	ifstream in;
-	in.open("maze.txt");
-	char line;
+    //required variables
+    ifstream in;
+    in.open("maze.txt");
+    char line;
 
-	//read the matrix using plain c code, character by character
-	in >> myMaze.rows;
-	in >> line;
-	in >> myMaze.cols;
-	cout << "Reading a " << myMaze.rows << " by " << myMaze.cols << " matrix." << endl;
-	//Burn the end of line character
-	in.ignore(200,'\n');
-	for(int i=0; i<myMaze.rows; i++)
-	{
-		for(int j=0; j<myMaze.cols; j++)
-		{
-			in.get( myMaze.matrix[i][j] );
-		}
-		//Burn the end of line character
-		in.ignore(200,'\n');
-	}
+    //read the matrix using plain c code, character by character
+    in >> myMaze.rows;
+    in >> line;
+    in >> myMaze.cols;
+    cout << "Reading a " << myMaze.rows << " by " << myMaze.cols << " matrix." << endl;
+    //Burn the end of line character
+    in.ignore(200,'\n');
+    for(int i=0; i<myMaze.rows; i++)
+    {
+        for(int j=0; j<myMaze.cols; j++)
+        {
+            in.get( myMaze.matrix[i][j] );
+        }
+        //Burn the end of line character
+        in.ignore(200,'\n');
+    }
 
-	//Print the empty maze
-	for(int i=0; i<myMaze.rows; i++)
-	{
-		for(int j=0; j<myMaze.cols; j++)
-		cout << myMaze.matrix[i][j];
-		cout << endl;
-	}
-	int x=1,y=1;
-	int endX=1,endY=1;
+    //Print the empty maze
+    for(int i=0; i<myMaze.rows; i++)
+    {
+        for(int j=0; j<myMaze.cols; j++)
+            cout << myMaze.matrix[i][j];
+        cout << endl;
+    }
+    int x=1,y=1;
+    int endX=1,endY=1;
 
-	//Find starting coordinates
-	for(int i=0; i<myMaze.rows; i++)
-	for(int j=0; j<myMaze.cols; j++)
-	if( myMaze.matrix[i][j] == 'S' ){
-		x=i;
-		y=j;
-	}
+    //Find starting coordinates
+    for(int i=0; i<myMaze.rows; i++)
+        for(int j=0; j<myMaze.cols; j++)
+            if( myMaze.matrix[i][j] == 'S' ){
+                x=i;
+                y=j;
+								myMaze.startX = x;
+								myMaze.startY = y;
+            }
 
-	Find Finish coordinates
-	for(int i=0; i<myMaze.rows; i++)
-	for(int j=0; j<myMaze.cols; j++)
-	if( myMaze.matrix[i][j] == 'F' ){
-		endX=j;
-		endY=i;
-	}
+    // Find Finish coordinates
+    // for(int i=0; i<myMaze.rows; i++)
+    // for(int j=0; j<myMaze.cols; j++)
+    // if( myMaze.matrix[i][j] == 'F' ){
+    //     endX=j;
+    //     endY=i;
+    // }
 
-	//Call a recursive mazeSolver
-	FIXME:RH:int bfDistance = bruteForceMazeSolver(x,y);     //brute force? dnc?
-	int btDistance = backtrackingMazeSolver(x,y);     //brute force? dnc?
-	// int gDistance = greedyMazeSolver(x,y,endX,endY);
-	int dncDistance = divideAndConquerMazeSolver(x,y);
-	int dpDistance = dynamicProgrammingMazeSolver(x,y);
-	int rDistance = randomizedMazeSolver(x,y);
+    //Call a recursive mazeSolver
+    //FIXME:RH:int bfDistance = bruteForceMazeSolver(x,y);     //brute force? dnc?
+    //int btDistance = backtrackingMazeSolver(x,y);     //brute force? dnc?
+    // int gDistance = greedyMazeSolver(x,y,endX,endY);
+    int dncDistance = divideAndConquerMazeSolver(x,y);
+    int dpDistance = dynamicProgrammingMazeSolver(x,y);
+    int rDistance = randomizedMazeSolver(x,y);
 
-	cout << "Brute force distance: " << bfDistance << " units away!" << endl;
-	cout << "Backtracking distance: " << btDistance << " units away!" << endl;
-	// cout << "Greedy distance: " << gDistance << " units away!" << endl;
-	cout << "Divide and conquer distance: " << dncDistance << " units away!" << endl;
-	cout << "Dynamic programming distance: " << dpDistance << " units away!" << endl;
-	cout << "Randomized distance: " << rDistance << " units away!" << endl;
+    //cout << "Brute force distance: " << bfDistance << " units away!" << endl;
+    //cout << "Backtracking distance: " << btDistance << " units away!" << endl;
+    // cout << "Greedy distance: " << gDistance << " units away!" << endl;
+    cout << "Divide and conquer distance: " << dncDistance << " units away!" << endl;
+    cout << "Dynamic programming distance: " << dpDistance << " units away!" << endl;
+    cout << "Randomized distance: " << rDistance << " units away!" << endl;
 
-	//Print solved maze - x2
-	// for(int i=0; i<myMaze.rows; i++)
-	// {
-	// 	for(int j=0; j<myMaze.cols; j++)
-	// 	cout << myMaze.matrix[i][j];
-	// 	cout << endl;
-	// }
+    //Print solved maze - x2
+    // for(int i=0; i<myMaze.rows; i++)
+    // {
+    //     for(int j=0; j<myMaze.cols; j++)
+    //     cout << myMaze.matrix[i][j];
+    //     cout << endl;
+    // }
 
-	// ****************************************
-	// Begin Student Written Section
-	// ****************************************
+    // ****************************************
+    // Begin Student Written Section
+    // ****************************************
 
 
-	char **mazeArray;
-	bool alwaysTrue = true;
+    // char **mazeArray = (char**)malloc(myMaze.rows * sizeof(char*));
+    // for(int i=0 ; i<myMaze.rows ; i++){
+    //     mazeArray[i] = (char*)malloc(myMaze.cols * sizeof(char*));
+    // }
 
-	mazeArray = new char *[myMaze.rows];
-	for(int i=0 ; i<myMaze.rows ; i++){
-		mazeArray[i] = new char[myMaze.cols];
-		for(int j=0 ; j<myMaze.cols ; j++)
-		mazeArray[i][j] = myMaze.matrix[i][j];
-	}
 
-	bruteForceMazeSolver(mazeArray, 1, 1, !alwaysTrue, x, y, myMaze);
+    bruteForceMazeSolver(1, 1, false, x, y);
 
-	return 0;
+
+    return 0;
 }
 
-int bruteForceMazeSolver(char **myMaze, int i, int j, bool oneShot, int startX, int startY, struct maze structMaze)
+int bruteForceMazeSolver(int i, int j, bool oneShot, int startX, int startY)
 {
-	bool isEmptySpace, isTraveledSpace;
+    bool isEmptySpace, isTraveledSpace;
 
-	bruteForceCount++;
+    bruteForceCount++;
 
-	// will not activate unless this is the first iteration
-	// gives starting location
-	if(!oneShot){
-		i = startX;
-		j = startY;
-		oneShot = true;
-		myMaze[i][j] = 't';
-	}
+    // will not activate unless this is the first iteration
+    // gives starting location
+    if(!oneShot){
+        i = startX;
+        j = startY;
+        oneShot = true;
+        myMaze.matrix[i][j] = '.';
+    }
 
-	// Check if adjacent spot is F
-	if(isFinishAdjacent(myMaze,i,j)){
-		myMaze[i][j] = 't';
-		printArray(myMaze, structMaze.rows, structMaze.cols);
-		cout << "Brute distance: " << bruteForceCount << endl;
-	}
-	else{
-		// if a finish spot is not nearby, check for empty space
-		isEmptySpace = bruteCheckForEmpty(myMaze,i,j,structMaze);
-		if(!isEmptySpace){
-			// if there are no empty spaces, check for already traveled spaces
-			isTraveledSpace = bruteCheckForTraveled(myMaze,i,j, structMaze);
-			if(!isTraveledSpace){
-				cout << "ERROR";
-			}
-		}
-	}
+    cout << endl << endl;
 
-	return -1;
+
+    // Check if adjacent spot is F
+    if(isFinishAdjacent(i,j)){
+        myMaze.matrix[i][j] = '.';
+				myMaze.matrix[myMaze.startX][myMaze.startY] = 'S';
+				printArray(myMaze);
+        cout << "Brute distance: " << bruteForceCount << endl;
+    }
+    else{
+        // if a finish spot is not nearby, check for empty space
+        isEmptySpace = bruteCheckForEmpty(i,j);
+        if(!isEmptySpace){
+            // if there are no empty spaces, check for already traveled spaces
+            isTraveledSpace = bruteCheckForTraveled(i,j);
+            if(!isTraveledSpace){
+                cout << "ERROR";
+            }
+        }
+    }
+
+    return -1;
 }
 int backtrackingMazeSolver(int i, int j)
 {
-	//algorithm goes here
-	return -1;
+    //algorithm goes here
+    return -1;
 }
-int greedyMazeSolver(int i, int j, int endX, int endY)
-{
-	if(myMaze.matrix[i][j] == 'F') return 1;
-
-	std::vector<int> nextNode = GetClosestNodeToFinish(i, j, endX, endY);
-	myMaze.matrix[nextNode[0]][nextNode[1]] = 'X';
-	return greedyMazeSolver(nextNode[0], nextNode[1], endX, endY);
-
-  return -1;
-}
-
-std::vector<int> GetClosestNodeToFinish(int i, int j, int endX, int endY)
-{
-	std::vector<int> north = {i, j-1};
-	std::vector<int> east = {i+1, j};
-	std::vector<int> south = {i, j+1};
-	std::vector<int> west = {i-1, j};
-
-	std::vector<std::vector<int>> directions;
-	directions.push_back(north);
-	directions.push_back(east);
-	directions.push_back(south);
-	directions.push_back(west);
-
-	std::vector<int> clostestNode = south;
-	for(int i = 0; i < 4; i++)
-	{
-		int nodeDistance = std::abs(clostestNode[0] - endX) + std::abs(clostestNode[1] - endY);
-		if(nodeDistance > (std::abs(directions[i][0] - endX) + std::abs(directions[i][1] - endY)))
-		{
-			if(myMaze.matrix[directions[i][0]][directions[i][1]] != '*')
-			{
-				clostestNode = directions[i];
-			}
-		}
-	}
-	return clostestNode;
-}
+// int greedyMazeSolver(int i, int j, int endX, int endY)
+// {
+//     if(myMaze.matrix[i][j] == 'F') return 1;
+//
+//     std::vector<int> nextNode = GetClosestNodeToFinish(i, j, endX, endY);
+//     myMaze.matrix[nextNode[0]][nextNode[1]] = '@';
+//     return greedyMazeSolver(nextNode[0], nextNode[1], endX, endY);
+//
+//   return -1;
+// }
+//
+// std::vector<int> GetClosestNodeToFinish(int i, int j, int endX, int endY)
+// {
+//     std::vector<int> north = {i, j-1};
+//     std::vector<int> east = {i+1, j};
+//     std::vector<int> south = {i, j+1};
+//     std::vector<int> west = {i-1, j};
+//
+//     std::vector<std::vector<int>> directions;
+//     directions.push_back(north);
+//     directions.push_back(east);
+//     directions.push_back(south);
+//     directions.push_back(west);
+//
+//     std::vector<int> clostestNode = south;
+//     for(int i = 0; i < 4; i++)
+//     {
+//         int nodeDistance = std::abs(clostestNode[0] - endX) + std::abs(clostestNode[1] - endY);
+//         if(nodeDistance > (std::abs(directions[i][0] - endX) + std::abs(directions[i][1] - endY)))
+//         {
+//             if(myMaze.matrix[directions[i][0]][directions[i][1]] != '*')
+//             {
+//                 clostestNode = directions[i];
+//             }
+//         }
+//     }
+//     return clostestNode;
+// }
 int divideAndConquerMazeSolver(int i, int j)
 {
-	//algorithm goes here
-	return -1;
+    //algorithm goes here
+    return -1;
 }
 int dynamicProgrammingMazeSolver(int i, int j)
 {
-	//algorithm goes here
-	return -1;
+    //algorithm goes here
+    return -1;
 }
 int randomizedMazeSolver(int i, int j)
 {
-	//algorithm goes here
-	return -1;
+    //algorithm goes here
+    return -1;
 }
 
 // Added by RH
@@ -224,153 +229,158 @@ int randomizedMazeSolver(int i, int j)
 // will check the character stored is a space character
 // (not in the Master Chief sorta way), then set the return
 // condition to true
-bool isEmpty(char **myMaze, int i, int j, int height, int width){
-	bool isEmpty = false;
-	bool isValidLocation = true;
+bool isEmpty(int i, int j){
+    bool isEmpty = false;
+    bool isValidLocation = true;
 
-	// check if valid
-	if(height < i){
-		cout << "Not a valid row lacation";
-		isValidLocation = !isValidLocation;
-	}
-	if(width < j){
-		cout << "Not a valid col lacation";
-		isValidLocation = !isValidLocation;
-	}
+    // check if valid
+    if(myMaze.rows < i){
+        cout << "Not a valid row lacation";
+        isValidLocation = !isValidLocation;
+    }
+    if(myMaze.cols < j){
+        cout << "Not a valid col lacation";
+        isValidLocation = !isValidLocation;
+    }
 
-	// check if space
-	if(isValidLocation){
-		if(myMaze[i][j] == ' ')
-		{
-			isEmpty = true;
-		}
-	}
+    // check if space
+    if(isValidLocation){
+        if(myMaze.matrix[i][j] == ' ')
+        {
+            isEmpty = true;
+        }
+    }
 
-	return isEmpty;
+    return isEmpty;
 
 }
 
 // Added by RH
-// Same as isValid, but for check the char 't'
-bool hasBeenChecked(char **myMaze, int i, int j, int height, int width){
-	bool isTracked = false;
-	bool isValidLocation = true;
+// Same as isValid, but for check the char '.'
+bool hasBeenChecked(int i, int j){
+    bool isTracked = false;
+    bool isValidLocation = true;
 
-	// check if valid
-	if(height < i){
-		cout << "Not a valid row lacation";
-		isValidLocation = !isValidLocation;
-	}
-	if(width < j){
-		cout << "Not a valid col lacation";
-		isValidLocation = !isValidLocation;
-	}
+    // check if valid
+    if(myMaze.rows < i){
+        cout << "Not a valid row lacation";
+        isValidLocation = !isValidLocation;
+    }
+    if(myMaze.cols < j){
+        cout << "Not a valid col lacation";
+        isValidLocation = !isValidLocation;
+    }
 
 
-	// check if space
-	if(isValidLocation){
-		if(myMaze[i][j] == 't'){
-			isTracked = true;
-		}
-	}
+    // check if space
+    if(isValidLocation){
+        if(myMaze.matrix[i][j] == '.'){
+            isTracked = true;
+        }
+    }
 
-	return isTracked;
+    return isTracked;
 
 }
 
-void printArray(char **array, int row, int col){
-	for(int i=0 ; i<row ; i++){
-		for(int j=0 ; j<col ; j++){
-			cout << array[i][j];
-		}
-		cout << endl;
-	}
+void printArray(struct maze array){
+    for(int i=0 ; i<myMaze.rows ; i++){
+        for(int j=0 ; j<myMaze.cols ; j++){
+            cout << array.matrix[i][j];
+        }
+        cout << endl;
+    }
 }
 
 
 // Checks for empty spots in direction order east, north, south, west
-// Marks a 't' on every traveled spot
-bool bruteCheckForEmpty(char **myMaze, int i, int j, struct maze structMaze){
-	if(isEmpty(myMaze,i,j+1,structMaze.rows,structMaze.cols)){
-		myMaze[i][j] = 't';
-		bruteForceMazeSolver(myMaze, i, j+1, true, 0, 0, structMaze);
-	}
-	else{
-		if(isEmpty(myMaze,i+1,j,structMaze.rows,structMaze.cols)){
-			myMaze[i][j] = 't';
-			bruteForceMazeSolver(myMaze, i+1, j, true, 0, 0, structMaze);
-		}
-		else{
-			if(isEmpty(myMaze,i-1,j,structMaze.rows,structMaze.cols)){
-				myMaze[i][j] = 't';
-				bruteForceMazeSolver(myMaze, i-1, j, true, 0, 0, structMaze);
-			}
-			else{
-				if(isEmpty(myMaze,i,j-1,structMaze.rows,structMaze.cols)){
-					myMaze[i][j] = 't';
-					bruteForceMazeSolver(myMaze, i, j-1, true, 0, 0, structMaze);
-				}
-				else{
-					return false;
-				}
-			}
-		}
-	}
-	return true;
+// Marks a '.' on every traveled spot
+bool bruteCheckForEmpty(int i, int j){
+    if(isEmpty(i,j+1)){
+        myMaze.matrix[i][j] = '.';
+        bruteForceMazeSolver(i, j+1, true, 0, 0);
+    }
+    else{
+        if(isEmpty(i+1,j)){
+            myMaze.matrix[i][j] = '.';
+            bruteForceMazeSolver(i+1, j, true, 0, 0);
+        }
+        else{
+            if(isEmpty(i-1,j)){
+                myMaze.matrix[i][j] = '.';
+                bruteForceMazeSolver(i-1, j, true, 0, 0);
+            }
+            else{
+                if(isEmpty(i,j-1)){
+                    myMaze.matrix[i][j] = '.';
+                    bruteForceMazeSolver(i, j-1, true, 0, 0);
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
 }
 
 
-// Runs the same thing as bruteCheckForEmpty, except places 'x' and
+// Runs the same thing as bruteCheckForEmpty, except places '@' and
 // does not run if the finish is adjacent. Without the check, the program
-// defaults to looking for 't' spaces rather than stopping
-bool bruteCheckForTraveled(char **myMaze, int i, int j, struct maze structMaze){
-	if(!isFinishAdjacent(myMaze,i,j)){
-		if(hasBeenChecked(myMaze,i,j+1,structMaze.rows,structMaze.cols)){
-			myMaze[i][j] = 'x';
-			bruteForceMazeSolver(myMaze, i, j+1,true, 0, 0, structMaze);
-		}
-		else{
-			if(hasBeenChecked(myMaze,i+1,j,structMaze.rows,structMaze.cols)){
-				myMaze[i][j] = 'x';
-				bruteForceMazeSolver(myMaze, i+1, j, true, 0, 0, structMaze);
-			}
-			else{
-				if(hasBeenChecked(myMaze,i-1,j,structMaze.rows,structMaze.cols)){
-					myMaze[i][j] = 'x';
-					bruteForceMazeSolver(myMaze, i-1, j, true, 0, 0, structMaze);
-				}
-				else{
-					if(hasBeenChecked(myMaze,i,j-1,structMaze.rows,structMaze.cols)){
-						myMaze[i][j] = 'x';
-						bruteForceMazeSolver(myMaze, i, j-1, true, 0, 0, structMaze);
-					}
-					else{
-						return false;
-					}
-				}
-			}
-		}
-	}
-	return true;
+// defaults to looking for '.' spaces rather than stopping
+bool bruteCheckForTraveled(int i, int j){
+    //if(!isFinishAdjacent(myMaze,i,j)){
+        if(hasBeenChecked(i,j+1)){
+            myMaze.matrix[i][j] = '@';
+            cout << "east " << i << "x" << j << endl;
+            bruteForceMazeSolver(i, j+1,true, 0, 0);
+        }
+        else{
+            if(hasBeenChecked(i+1,j)){
+                myMaze.matrix[i][j] = '@';
+                cout << "north " << i << "x" << j << endl;
+                bruteForceMazeSolver(i+1, j, true, 0, 0);
+            }
+            else{
+                if(hasBeenChecked(i-1,j)){
+                    myMaze.matrix[i][j] = '@';
+                    cout << "south " << i << "x" << j  << endl;
+                    bruteForceMazeSolver(i-1, j, true, 0, 0);
+                }
+                else{
+                    if(hasBeenChecked(i,j-1)){
+                        myMaze.matrix[i][j] = '@';
+                        cout << "west" << i << "x" << j  << endl;
+                        bruteForceMazeSolver(i, j-1, true, 0, 0);
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+    //}
+    return true;
 }
 
 // checks all directions for adjacent 'F'
-bool isFinishAdjacent(char **myMaze, int i, int j){
+bool isFinishAdjacent(int i, int j){
 
-	if(myMaze[i+1][j]=='F'){
-		return true;
-	}
-	if(myMaze[i-1][j]=='F'){
-		return true;
-	}
-	if(myMaze[i][j+j]=='F'){
-		return true;
-	}
-	if(myMaze[i][j-j]=='F'){
-		return true;
-	}
 
-	return false;
+    if(myMaze.matrix[i+1][j]=='F'){
+        return true;
+    }
+    if(myMaze.matrix[i-1][j]=='F'){
+        return true;
+    }
+    if(myMaze.matrix[i][j+1]=='F'){
+        return true;
+    }
+    if(myMaze.matrix[i][j-1]=='F'){
+        return true;
+    }
+
+    return false;
 }
 
 //recursion!!
